45c48f197ecb4cdbbca488d8001e430b12859e07
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-02-03 14:56:34 -0800

    added add.py file

---
+++ Task 1.3/add.py
@@ -0,0 +1,11 @@
+a = input("Enter a number: ")
+b = input("Enter another number to be added to the firsta; ")
+a = int(a)
+b = int(b)
+
+print("The sum of these numbers is", a + b)
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number tobe added to the first: "))
+
+print("The sum if these numbers is " + str(a + b))

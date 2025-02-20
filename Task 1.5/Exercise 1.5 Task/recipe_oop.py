0323ff774ef44979e44a6459fc204420b850ce7d
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-02-19 21:01:12 -0800

    updated

---
+++ Task 1.5/Exercise 1.5 Task/recipe_oop.py
@@ -0,0 +1,8 @@
+class Recipe(object): 
+    def __init__(self, name, ingredients, cooking_time, difficulty):
+        self.name = name
+        self.ingredients = ingredients
+        self.cooking_time = cooking_time
+        self.difficulty = difficulty
+
+        

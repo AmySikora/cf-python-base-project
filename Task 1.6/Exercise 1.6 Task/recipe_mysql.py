e44d0b0c4c66aaa9003ef7c471df0d70fee9b064
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-02-24 17:19:57 -0800

    converted ingredients into a comma-separated string

--- Task 1.6/Exercise 1.6 Task/recipe_mysql.py
+++ Task 1.6/Exercise 1.6 Task/recipe_mysql.py
@@ -1,3 +1,70 @@
+# Defines cooking difficulty    
+def calc_difficulty(cooking_time, num_ingredients): 
+    if cooking_time < 10 and num_ingredients < 4:
+        return "Easy"
+    elif cooking_time < 10 and num_ingredients >= 4:
+        return "Medium"
+    elif cooking_time >= 10 and num_ingredients < 4:
+        return "Intermediate"
+    else:
+        return "Hard"
+    
+# Define create recipe
+def create_recipe(conn, cursor):
+    name = input("\nEnter recipe name: ").strip()
+    
+    # Verify cooking time input is a number
+    while True: 
+        try:
+            cooking_time = int(input("Enter cooking time in minutes: "))
+            break
+        except ValueError:
+            print("Only numerical values can be entered. Please enter a number.")
+
+    # Get ingredients
+    ingredients = []
+    while True:
+        try:
+            n = int(input("Enter the number of ingredients: "))
+            if n > 0:
+                break
+            else:
+                print("Please enter a positive number.")
+        except ValueError:
+            print("Please enter a valid number.")   
+
+    for _ in range(n):
+        ingredient = input("Enter an ingredient: ").strip().lower()
+        if ingredient not in ingredients:
+            ingredients.append(ingredient)
+        else:
+            print(f"{ingredient} is already in the list. Skipping duplicate.")
+
+    # Convert ingredients list to a comma-separated string
+    ingredients_str = ", ".join(ingredients)
+
+    # Calculate cooking difficulty 
+    difficulty = calc_difficulty(cooking_time, len(ingredients))
+
+    # SQL Query to Insert Data into the Recipes Table
+    query = '''INSERT INTO recipes (name, ingredients, cooking_time, difficulty) 
+               VALUES (%s, %s, %s, %s)'''
+    
+    values = (name, ingredients_str, cooking_time, difficulty)
+
+    # Execute query
+    cursor.execute(query, values)
+    conn.commit()  # Save changes to the database
+
+    print("\nRecipe added successfully!")
+
+    return {
+        'name': name,
+        'cooking_time': cooking_time,
+        'ingredients': ingredients,
+        'difficulty': difficulty
+    }
+
 # This is the loop running in the main menu
 # Loop continues as long as user doesn't quit
 def main_menu(conn, cursor):
@@ -46,5 +113,3 @@ cursor = conn.cursor()
 
 # Start the menu
 main_menu(conn, cursor)
-
-

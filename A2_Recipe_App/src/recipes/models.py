2fb05bb1a89889d4a08a18ccccc112f11975ac33
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-12 20:01:48 -0700

    updated to correct typo

--- A2_Recipe_App/src/recipes/models.py
+++ A2_Recipe_App/src/recipes/models.py
@@ -6,7 +6,7 @@ class Recipe(models.Model):
     description = models.TextField(blank=True, null=True)
     ingredients = models.TextField(help_text="List ingredients seperated by commas")
     cooking_time = models.PositiveIntegerField(help_text="Cooking time in minutes")
-    difficulty = models.CharField(max_lenght=20, blank=True, null=True)
+    difficulty = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.name

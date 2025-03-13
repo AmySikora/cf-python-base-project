dd70379aaf6d8143e07be25c042c8a17cbbc2171
AmySikora <101671204+AmySikora@users.noreply.github.com>
2025-03-12 19:10:05 -0700

    registering recipes app

--- A2_Recipe_App/src/recipe_project/settings.py
+++ A2_Recipe_App/src/recipe_project/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Recipe Project Apps
+    'recipes'
 ]
 
 ROOT_URLCONF = 'recipe_project.urls'
